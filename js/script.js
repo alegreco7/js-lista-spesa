9189e3d7df5eaf16bd0912af0cacff092f95f43d
alegreco7 <alexgreco2002@gmail.com>
2023-11-27 17:35:44 +0100

    ciclo lista della spesa con while

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let lista_spesa = ["uova", "latte", "cereali", "pane"]
+
+let i = 0
+let lenLista = listaSpesa.length;
+
+while (i < lenLista) {
+    document.write(` ${listaSpesa[i]}`)
+    i++;
+}
